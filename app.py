c8686d8793be929b3b76792b2bed096831cf2063
Rohith <rohithgeorgeninan@gmail.com>
2022-08-09 13:24:47 +0530

    API now accepts audio file instead of path

--- app.py
+++ app.py
@@ -1,21 +1,35 @@
 from flask import Flask, request
+from werkzeug.utils import secure_filename
 from flask.json import jsonify
+import os.path
 
 from stt import stt
 from keyword_extract import get_keywords_v
 
 app = Flask("__name__")
 
+temp_path = './temp'
+
 @app.route("/keywords", methods=['POST'])
 def keywords():
-    input_json = request.get_json(force=True) 
+    file = request.files['audio']
 
-    path = input_json['path']
-    transcript = stt(path)
+    if file:
+            filename = secure_filename(file.filename)
+
+            if not os.path.exists(temp_path):
+                os.makedirs(temp_path)
+
+            audio_path = os.path.join(temp_path, filename)
+            file.save(audio_path)
+
+    transcript = stt(audio_path)
 
     keywords = get_keywords_v(list(transcript), num=3)
 
-    return keywords
+    ret = {'keywords': keywords}
+
+    return (jsonify(ret))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- split_audio.py
+++ split_audio.py
@@ -1,15 +1,16 @@
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
+import os.path
 
 def split_audio(file_path,out_dir,silent_duration=500,thresh=-55):
-  sound_file = AudioSegment.from_file(file_path)
-  audio_chunks = split_on_silence(sound_file, 
-      min_silence_len=silent_duration,
-      # consider it silent if quieter than -16 dBFS
-      silence_thresh=thresh
-  )
+    sound_file = AudioSegment.from_file(file_path)
+    audio_chunks = split_on_silence(sound_file, 
+        min_silence_len=silent_duration,
+        # consider it silent if quieter than -16 dBFS
+        silence_thresh=thresh
+    )
 
-  for i, chunk in enumerate(audio_chunks):
-      out_file = os.path.join(out_dir,"chunk{0}.wav".format(i))
-      print("exporting", out_file)
-      chunk.export(out_file, format="wav")
+    for i, chunk in enumerate(audio_chunks):
+        out_file = os.path.join(out_dir,"chunk{0}.wav".format(i))
+        print("exporting", out_file)
+        chunk.export(out_file, format="wav")
