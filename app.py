52059a1169f5e612f172d49aa0cb2d3df66e4c0a
Rohith <rohithgeorgeninan@gmail.com>
2022-08-23 20:40:36 +0530

    Different endpoints for docx and pdf

--- app.py
+++ app.py
@@ -1,71 +1,46 @@
-from flask import Flask, request, send_from_directory
-from werkzeug.utils import secure_filename
-from flask.json import jsonify
-import os
+from flask import Flask, request
+from flask_cors import CORS
 
-from vid_downloader import download_as_wav
-from helper import random_folder, folder_cleanup, audio_to_docx, transcripts_to_pdf
-from yt_transcript import video_id, getTranscript
+from helper import audio_processing, yt_processing
 
 app = Flask("__name__")
+CORS(app)
 
-temp_path = './temp'
-
-@app.route("/audio", methods=['POST'])
-def audio():
+@app.route("/audiotopdf", methods=['POST'])
+def audiotopdf():
     '''
     Basic Usage:
-    curl -F audio=@new1.wav http://http://127.0.0.1:5000/audio -o notes.pdf
+    curl -F audio=@new1.wav http://http://127.0.0.1:5000/audiotopdf -o notes.pdf
     '''
-    file = request.files['audio']
+    
+    return audio_processing(request, to_pdf=True)
 
-    if not file:
-        print("Invalid file")
-        return
-
-    folder_name = random_folder(temp_path)
-
-    filename = secure_filename(file.filename)
-    audio_path = os.path.join(folder_name, filename)
-    file.save(audio_path)
-
-    pdf_path = audio_to_docx(folder_name, audio_path)
-
-    response = send_from_directory(directory='.', path=pdf_path)
-
-    folder_cleanup(folder_name)
-
-    return response
-
-@app.route("/yt", methods=['POST'])
-def yt():
+@app.route("/audiotodocx", methods=['POST'])
+def audiotodocx():
     '''
     Basic Usage:
-    curl -F url=https://www.youtube.com/watch?v=dQw4w9WgXcQ http://localhost:5000/yt
+    curl -F audio=@new1.wav http://http://127.0.0.1:5000/audiotodocx -o notes.docx
     '''
-    url = request.form.get("url")
+    
+    return audio_processing(request, to_pdf=False)
 
-    vid_id = video_id(url)
-    if not vid_id:
-        print("Invalid URL")
-        return jsonify([])
+@app.route("/yttopdf", methods=['POST'])
+def yttopdf():
+    '''
+    Basic Usage:
+    curl -F url=https://www.youtube.com/watch?v=dQw4w9WgXcQ http://localhost:5000/yttopdf -o notes.pdf
+    '''
+    
+    return yt_processing(request, to_pdf=True)
 
-    folder_name = random_folder(temp_path)
-
-    extract_transcription = getTranscript(vid_id)
-
-    if extract_transcription:
-        transcript = getTranscript(vid_id)
-        pdf_path = transcripts_to_pdf(folder_name, [transcript])
-    else:
-        audio_path = download_as_wav(url, folder_name)
-        pdf_path = audio_to_docx(folder_name, audio_path)
-
-    response = send_from_directory(directory='.', path=pdf_path)
-
-    folder_cleanup(folder_name)
-
-    return response
+@app.route("/yttodocx", methods=['POST'])
+def yttodocx():
+    '''
+    Basic Usage:
+    curl -F url=https://www.youtube.com/watch?v=dQw4w9WgXcQ http://localhost:5000/yttodocx -o notes.docx
+    '''
+    
+    return yt_processing(request, to_docx=False)
 
 @app.route("/")
 def index():
--- helper.py
+++ helper.py
@@ -2,6 +2,9 @@ import os
 import random
 import string
 import shutil
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
+from flask.json import jsonify
 
 from stt import stt
 from keyword_extract import get_keywords_v
@@ -10,6 +13,10 @@ from wikipedia_links import get_nlinks, get_images
 from generate_docx import to_docx
 from generate_pdf import to_pdf
 from punctuate import punctaute
+from yt_transcript import video_id, getTranscript
+from vid_downloader import download_as_wav
+
+temp_path = './temp'
 
 def random_folder(temp_path):
     folder_name = os.path.join(temp_path, ''.join(random.choices(string.ascii_letters + string.digits, k=20)))
@@ -26,7 +33,13 @@ def folder_cleanup(folder_name):
         print(f"Failed to delete directory {folder_name}")
         print(e)
 
-def transcripts_to_pdf(folder_name, transcript_list,punctuation=False):
+def docx_to_pdf(docx_path):
+    to_pdf(docx_path)
+
+    pdf_path = os.path.splitext(docx_path)[0] + '.pdf'
+    return pdf_path
+
+def transcripts_to_docx(folder_name, transcript_list,punctuation=False):
     if punctuation:
         transcript_list = [punctaute(x) for x in transcript_list if x != ' ' and len(x.split()) > 1]
     concat_transcript = ' '.join(transcript_list)
@@ -38,10 +51,8 @@ def transcripts_to_pdf(folder_name, transcript_list,punctuation=False):
     image_links = get_images(keywords, file_path=os.path.join(folder_name, 'images/'))
     image_content = list(zip(list(image_links.values()),list(image_links.keys())))
     docx_path = to_docx(keywords[0].title(), transcript_list, keywords, image_content, get_nlinks(keywords), output_directory=folder_name)
-    to_pdf(docx_path)
 
-    pdf_path = os.path.splitext(docx_path)[0] + '.pdf'
-    return pdf_path
+    return docx_path
 
 def audio_to_docx(folder_name, audio_path):
     split_audio_path = os.path.join(folder_name, 'split_files/')
@@ -54,5 +65,60 @@ def audio_to_docx(folder_name, audio_path):
     chunk_paths.sort()
     transcripts = stt(chunk_paths)
 
-    pdf_path = transcripts_to_pdf(folder_name, transcripts,punctuation=True)
-    return pdf_path
+    docx_path = transcripts_to_docx(folder_name, transcripts,punctuation=True)
+
+    return docx_path
+
+def audio_processing(request, to_pdf=True):
+    file = request.files['audio']
+
+    if not file:
+        print("Invalid file")
+        return
+
+    folder_name = random_folder(temp_path)
+
+    filename = secure_filename(file.filename)
+    audio_path = os.path.join(folder_name, filename)
+    file.save(audio_path)
+
+    docx_path = audio_to_docx(folder_name, audio_path)
+
+    if to_pdf:
+        pdf_path = docx_to_pdf(docx_path)
+        response = send_from_directory(directory='.', path=pdf_path)
+    else:
+        response = send_from_directory(directory='.', path=docx_path)
+
+    folder_cleanup(folder_name)
+
+    return response
+
+def yt_processing(request, to_pdf=True):
+    url = request.form.get("url")
+
+    vid_id = video_id(url)
+    if not vid_id:
+        print("Invalid URL")
+        return jsonify([])
+
+    folder_name = random_folder(temp_path)
+
+    extract_transcription = getTranscript(vid_id)
+
+    if extract_transcription:
+        transcript = getTranscript(vid_id)
+        docx_path = transcripts_to_docx(folder_name, [transcript])
+    else:
+        audio_path = download_as_wav(url, folder_name)
+        docx_path = audio_to_docx(folder_name, audio_path)
+
+    if to_pdf:
+        pdf_path = docx_to_pdf(docx_path)
+        response = send_from_directory(directory='.', path=pdf_path)
+    else:
+        response = send_from_directory(directory='.', path=docx_path)
+
+    folder_cleanup(folder_name)
+
+    return response
