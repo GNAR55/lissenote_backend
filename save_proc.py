88d00ffb64480db52c13a1e9514ca510d2133bd5
AkshayWarrier <aksmen121@gmail.com>
2022-08-25 10:51:32 +0530

    remove unwanted files

--- save_proc.py
+++
@@ -1,3 +0,0 @@
-from transformers import Wav2Vec2ProcessorWithLM
-processor = Wav2Vec2ProcessorWithLM.from_pretrained("jonatasgrosman/wav2vec2-large-xlsr-53-english")
-processor.save_pretrained("./test")
